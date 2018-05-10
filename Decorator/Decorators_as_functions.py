dd994c4098fb3e633f961e788cc844822aa3e264
touhid <touhidalamin123@gmail.com>
2018-05-10 13:00:57 +0600

    added argument in decoretore

--- Decorator/Decorators_as_functions.py
+++ Decorator/Decorators_as_functions.py
@@ -1,18 +1,42 @@
-def be_polite(fn):
-    def wrapper():
-        print("What a pleasure to meet you!")
-        fn()
-        print("Have a greet day!")
+# def be_polite(fn):
+#     def wrapper():
+#         print("What a pleasure to meet you!")
+#         fn()
+#         print("Have a greet day!")
+#     return wrapper
+#
+# @be_polite
+# def greet():
+#     print("My name is Touhid")
+#
+# @be_polite
+# def rage():
+#     print("I hate you!")
+#
+# greet()
+# print('------------------')
+# rage()
+#
+# # ploite_rage = be_polite(rage)
+# # greet()
+# # print()
+# # ploite_rage()
+
+# Decorator with different signature
+
+def shout(fn):
+    def wrapper(name):
+        return fn(name).upper()
     return wrapper
 
-@be_polite
-def greet():
-    print("My name is Touhid")
+@shout
+def greet(name):
+    return f'Hi I\'m {name}'
 
-@be_polite
-def rage():
-    print("I hate you!")
 
-greet()
-print('-------------------')
-rage()
+@shout
+def order(main, side):
+    return f"Hi, I'd like the {main}, with a side of {side}, please."
+
+
+print(greet('Touhid'))
